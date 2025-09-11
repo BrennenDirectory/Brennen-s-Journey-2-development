ce7132d7a7be991a4464c5c66b41270fbd5f65be
Brennen Directory <brennendirectory@gmail.com>
2025-09-10 23:17:57 -0400

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,42 @@
+_G.love = require("love")
+local source
+
+function love.load()
+    source = require("src.source")
+    source.load()
+    math.randomseed(os.time())
+end
+
+function love.update(dt)
+    if dt > 1 then
+        return
+    end
+    source.update(dt)
+end
+
+function love.draw()
+    source.draw()
+end
+
+function love.resize(width, height)
+    source.resize(width, height)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+    source.keypressed(key, scancode, isrepeat)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
+
+function love.mousepressed(x, y, button)
+    source.mousepressed(x, y, button)
+end
+
+function love.textinput(t)
+    source.textinput(t)
+end
+
+function love.quit()
+    source.quit()
+end
